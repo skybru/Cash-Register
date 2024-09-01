9f2fdbf8ed93601fd76e8d6885de4884db6a2a6d
skybru <giorgio.bruschi98@gmail.com>
2024-09-01 21:42:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
+
+const inputCash = document.getElementById("cash");
+const change = document.getElementById("change-due");
+const purchaseBtn = document.getElementById("purchase-btn");
