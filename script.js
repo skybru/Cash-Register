e107ad55f90f371a744a7ac009d7fd7b11560d01
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-09-13 15:36:32 +0200

    re-do of the script from scratch to manage status

--- script.js
+++ script.js
@@ -1,57 +1,106 @@
 let price = 1.87;
 let cid = [
-  ['PENNY', 1.01],    //$0.01 = 101 pennies
-  ['NICKEL', 2.05],   //$0.05 = 41 nickels
-  ['DIME', 3.1],      //$0.10 = 31 dimes
-  ['QUARTER', 4.25],  //$0.25 = 17
-  ['ONE', 90],        //$1.00 = 90
-  ['FIVE', 55],       //$5.00 = 11
-  ['TEN', 20],        //$10.00 = 2
-  ['TWENTY', 60],     //$20.00 = 3
-  ['ONE HUNDRED', 100]//$100.00 = 1
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
 ];
 
-let cidValues = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+const coinDenomArr = [
+  ["PENNY", 0.01],
+  ["NICKEL", 0.05],
+  ["DIME", 0.10],
+  ["QUARTER", 0.25],
+  ["ONE", 1],
+  ["FIVE", 5],
+  ["TEN", 10],
+  ["TWENTY", 20],
+  ["ONE HUNDRED", 100]
+]
 
+let denominations = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+let changeResult = {
+  status: "",
+  change: []
+};
+
+const changeDueEl = document.getElementById("change-due");
 const inputCash = document.getElementById("cash");
-const changeElement = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
 
-let cashArr = [];
-let change = 0;
-let modulus = 0;
-const cidReversed = cid.slice().reverse();
-const valuesReversed = cidValues.slice().reverse();
+const checkResult = () => {
+  //controllo sul valore inserito != 0
+  //nel caso valore inserito > 0 allora calcolo il resto
+  if (!inputCash.value) {
+    alert("Please, insert customer payment!");
+    return;
+  }
+  calculateChangeDue();
+};
 
-const calcChange = () => {
-  for (let i = 0; i < valuesReversed.length; i++) {
-    let counter = 0;
-    modulus = (change % (valuesReversed[i] * 100));
+const calculateChangeDue = () => {
+  console.log("calculateChangeDue function:");
+  //verifico se input è uguale al prezzo
+  if (Number(inputCash.value) === price) {
+    changeDueEl.innerHTML = 
+    `<p>No change due because customer paid with exact money</p>`;
+    inputCash.value = "";
+    return;
+  }
+  //o se è inferiore al prezzo
+  if (Number(inputCash.value) < price) {
+    alert("Customer does not have money to purchase item(s)");
+    return;
+  }
 
-    if (modulus != change) {
-      counter = Math.floor(change / (valuesReversed[i] * 100));
-      change = change - ((valuesReversed[i] * 100) * counter);
-      cashArr.push(counter * valuesReversed[i]);
-      cidReversed[i][1] -= cashArr[i];
-    } else {
-      cashArr.push(0);
-    }
-  };
+  //calcolo il resto e controllo lo stato
+  let change = parseFloat(inputCash.value - price).toFixed(2);
+  let status = checkStatus(change);
 
+  //se lo stato è valido procedo a calcolare le denominazioni da restituire
+  if (status) {
+    const denomReversed = denominations.slice().reverse();
+    changeDueEl.innerHTML += 
+    `<p>Change due: ${change}</p>`;
 
-  console.log(cashArr);
+    for (let i = 0; i < denomReversed.length; i++) {
+      
+    };
+  }
+};
 
+const checkStatus = (change) => {
+  console.log("checkStatus function:")
+  //sommo tutti i soldi che ho nel cassetto
+  let cidSum = 0;
+  const cidReversed = cid.slice().reverse();
+  cidReversed.forEach((item) => cidSum += item[1]);
+  console.log(cidSum);
+
+  //verifico se il mio cassetto non può restituire il resto necessario
+  if (cidSum < change) {
+    changeDueEl.innerHTML = 
+    `<p>Status: INSUFFICIENT_FUNDS</p>`;
+    return false;
+    
+  }
+  //o se ho appena finito i soldi nel cassetto
+  if (cidSum === change) {
+    changeDueEl.innerHTML = 
+    `<p>Status: CLOSED</p>`;
+    return false;
+  }
+
+  changeDueEl.innerHTML = 
+  `<p>Status: OPEN</p>`;
+  return true;
 };
 
 purchaseBtn.addEventListener("click", () => {
-
-  change = (inputCash.value * 100) - (price * 100);
-  console.log("Change: " + change);
-  calcChange();
-  updateCid();
-
-  //aggiungere che il contante entrante si aggiunge alla cassa
-  //console.log(cid[0][0]);  //string
-  //console.log(cid[0][1]);  //number
+  checkResult();
 });
-
